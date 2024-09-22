9ad7e945e5f7b1042008afb79dd5aeb531100c55
SADMANGaming <74666591+SADMANGaming@users.noreply.github.com>
2024-09-22 20:10:11 +0600

    Update downloads.js

--- commands/downloads/downloads.js
+++ commands/downloads/downloads.js
@@ -7,7 +7,7 @@ module.exports = {
         const initialEmbed = new MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Downloads!')
-            .setDescription('[COD 1.1](https://cod.pm/1.1)\n[COD 1.5](https://cod.pm/1.5/)\n[COD UO 1.51](https://cod.pm/1.51/)');
+            .setDescription('[COD 1.1](https://cod.pm/1.1)\n[COD 1.5](https://cod.pm/1.5/)\n[COD UO 1.51](https://cod.pm/1.51/)\nNOTE: COD UO requires COD 1.5 patch!');
 
         await message.reply({ embeds: [initialEmbed] });
     },
